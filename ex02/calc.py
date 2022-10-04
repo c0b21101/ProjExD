470c5ae246de10a46ea450c8e5071fc604c42e9f
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-04 16:10:44 +0900

    練習7おわた

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,3 +1,4 @@
+import numbers
 import tkinter as tk
 import tkinter.messagebox as tkm
 from turtle import right, width
@@ -9,15 +10,23 @@ def click_number(event):
     entry.insert(tk.END, num)
 
 
+def click_equal(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
+
+
 root = tk.Tk()
 root.geometry("300x500")
 
-entry = tk.Entry(root, width=10, font=("", 40), justify="right")
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right")
 entry.grid(row=0, column=0, columnspan=3)
 
-r, c = 1, 0
+r, c = 9, 0
+numbers = list(range(9, -1, -1))
 operators = ["+"]
-for i, num in enumerate(range(9,-1, -1,), 1):
+for i, num in enumerate(numbers+operators, 1):
     btn = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     btn.bind("<1>", click_number)
     btn.grid(row=r, column=c)
@@ -25,5 +34,9 @@ for i, num in enumerate(range(9,-1, -1,), 1):
     if i%3 == 0:
         r += 1
         c = 0
+
+btn = tk.Button(root, text=f"=", width=4, height=2, font=("", 30))
+btn.bind("<1>", click_equal)
+btn.grid(row=r, column=c)
 
 root.mainloop()        
