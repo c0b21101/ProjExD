2cec6b3a0ae9feb4e1dd65e85b4466ed53ff74f2
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-04 15:05:49 +0900

    練習2おわた

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,24 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+# def click_number(event):
+#     btn = event.widget
+#     num = int(btn["text"])
+#     tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+
 
 root = tk.Tk
 root.geometry("300x500")
+
+
+r, c = 1, 0
+for i, num in enumerate(range(9,-1, -1,), 1):
+    btn = tk.Button(root, widht=4, height=2, font=("Times New Roman", 30))
+    # btn.bind("<1>", click_number)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+
+root.mainloop()        
