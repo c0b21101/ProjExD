e8f943dd09cc232ad68ecf276e50e142af959d61
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-18 15:21:25 +0900

    練習11

--- ex03/maze.py
+++ ex03/maze.py
@@ -15,15 +15,17 @@ def key_up(event):
 
 #練習7
 def main_proc():
-    global cx, cy, key, mx, my
+    global mx, my
+    global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    cx, cy = mx*100+50, my*100+50
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -32,8 +34,9 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #練習1
     
+    #練習2
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
-    canv.pack()#練習2
+    canv.pack()
 
     #練習9
     maze_lst = mm.make_maze(15, 9)
@@ -43,7 +46,8 @@ if __name__ == "__main__":
     mm.show_maze(canv, maze_lst)
 
     tori = tk.PhotoImage(file="fig/0.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100, my*100 #練習11
     canv.create_image(cx, cy, image=tori, tag="tori") #練習3
     
     #練習4 
@@ -55,8 +59,5 @@ if __name__ == "__main__":
 
     #練習7
     main_proc()
-
-    #練習11
-    mx, my = 1, 1
-
+    
     root.mainloop()
