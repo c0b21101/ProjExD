a1df39daf256eb0f958bef3b2591e2c9ad9c6c03
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-18 15:18:26 +0900

    練習11

--- ex03/maze.py
+++ ex03/maze.py
@@ -15,7 +15,7 @@ def key_up(event):
 
 #練習7
 def main_proc():
-    global cx, cy, key
+    global cx, cy, key, mx, my
     if key == "Up":
         cy -= 20
     if key == "Down":
@@ -35,6 +35,13 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()#練習2
 
+    #練習9
+    maze_lst = mm.make_maze(15, 9)
+    # print(maze_lst)
+
+    #練習10
+    mm.show_maze(canv, maze_lst)
+
     tori = tk.PhotoImage(file="fig/0.png")
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori") #練習3
@@ -49,10 +56,7 @@ if __name__ == "__main__":
     #練習7
     main_proc()
 
-    #練習9
-    maze_lst = mm.make_maze(15, 9)
-    #print(maze_lst)
-    mm.show_maze(canv, maze_lst)
-
+    #練習11
+    mx, my = 1, 1
 
     root.mainloop()
