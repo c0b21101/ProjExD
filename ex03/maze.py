483803e87c784414c9dc1435eb8e8da9bb60e1cd
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-18 14:18:04 +0900

    練習2

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+from turtle import bgcolor
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん") #練習1
+    
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack()#練習2
 
     root.mainloop()
