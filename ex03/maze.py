65e950233853b4df22a1c6cf695e502760cb6b99
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-18 14:21:54 +0900

    練習3

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,4 +8,8 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()#練習2
 
+    tori = tk.PhotoImage(file="fig/0.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori") #練習3
+
     root.mainloop()
