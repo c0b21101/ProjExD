a01845a05009efdbe157c2c256728892bf603219
Takasaki Shogo <c0b21101c5@edu.teu.ac.jp>
2022-10-25 14:55:56 +0900

    練習問題04-6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,12 @@
 import pygame as pg
 import sys
+from random import randint
 
 def main():
+    # 練習1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
@@ -13,6 +16,14 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
+    # 練習5
+    bomb_sfc = pg.Surface((20, 20)) # 空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を投下させる
+    pg.draw.circle(bomb_sfc,(255, 0, 0), (10, 10), 10) # 円を描く
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = randint(0, scrn_rct.width)
+    bomb_rct.centery = randint(0, scrn_rct.height)
+
     clock = pg.time.Clock() # 練習1
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
@@ -20,7 +31,6 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return # 練習2
 
-        scrn_sfc.blit(tori_sfc, tori_rct)
 
         # 練習4
         key_states = pg.key.get_pressed()
@@ -28,6 +38,10 @@ def main():
         if key_states[pg.K_DOWN]:  tori_rct.centery +=1
         if key_states[pg.K_LEFT]:  tori_rct.centerx -= 1
         if key_states[pg.K_RIGHT]: tori_rct.centerx += 1
+
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
+
 
 
         pg.display.update() # 練習2
